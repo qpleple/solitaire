4af95f698a66274f5bf5ed90ef8678c1875e4176
Quentin Pleplé <quentin.pleple@gmail.com>
2011-08-02 11:58:39 +0200

    added error handling

---
+++ src/SolitaireException.php
@@ -0,0 +1,10 @@
+<?php
+
+define("NOT_A_COLOR", 1001);
+define("NOT_A_COLOR_MSG", "Not a color. Given: %s");
+
+define("NOT_A_NUMBER", 1001);
+define("NOT_A_NUMBER_MSG", "Not a number. Given: %s");
+
+
+class SolitaireException extends Exception { }
